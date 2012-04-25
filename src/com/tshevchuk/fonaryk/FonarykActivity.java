a9fd03946d5742a075b390b7e5fbffe744559821
tshevchuk <taras.shevchuk@gmail.com>
2012-04-25 19:17:49 +0300

    Full brightness, disable wake lock.

--- src/com/tshevchuk/fonaryk/FonarykActivity.java
+++ src/com/tshevchuk/fonaryk/FonarykActivity.java
@@ -2,11 +2,23 @@ package com.tshevchuk.fonaryk;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class FonarykActivity extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.a_fonaryk);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.a_fonaryk);
+
+		getWindow().addFlags(
+				WindowManager.LayoutParams.FLAG_FULLSCREEN
+						| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		setBrightness(100);
+	}
+	
+	private void setBrightness(int brightness) {
+	    WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+	    layoutParams.screenBrightness = brightness/100.0f;
+	    getWindow().setAttributes(layoutParams);
+	}
 }
