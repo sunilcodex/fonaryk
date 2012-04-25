795b7d375ea3a304741fdf73f800aee5a03ee6b6
tshevchuk <taras.shevchuk@gmail.com>
2012-04-25 18:50:17 +0300

    Show white screen

--- src/com/tshevchuk/fonaryk/FonarykActivity.java
+++ src/com/tshevchuk/fonaryk/FonarykActivity.java
@@ -4,10 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class FonarykActivity extends Activity {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.a_fonaryk);
     }
 }
